f75f1409df40a5f311b954ab3af1130b7874c2d0
Krish C Naik <krishnaik06@gmail.com>
2020-01-16 22:49:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 
 
 app = Flask(__name__)
-model = pickle.load(open('Data/Pickle_Models/randomForestRegressor.pkl','rb'))
+model = pickle.load(open('random_forest_regression_model.pkl','rb'))
 
 
 @app.route('/')
